bec6ad65cca80973b40f203ab1d0953445af39cb
Mateus Silva Mendes <mateussilvamendes3@gmail.com>
2026-09-08 00:35:36 -0300

    refactor: estrutura modular css e js

---
+++ js/tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        torii:   { DEFAULT:'#DC2626', dark:'#B01C1C', soft:'#FEE9E9' },
+        nanquim: { DEFAULT:'#18181B', 60:'#6B6B72', 40:'#A1A1AA' },
+        matcha:  { DEFAULT:'#059669', soft:'#E4F5EE' },
+        washi:   { DEFAULT:'#FAF8F5', deep:'#F1EDE6', line:'#E6E0D6' }
+      },
+      fontFamily: {
+        mincho: ['"Shippori Mincho"','serif'],
+        kaku:   ['"Zen Kaku Gothic New"','sans-serif']
+      }
+    }
+  }
+}
